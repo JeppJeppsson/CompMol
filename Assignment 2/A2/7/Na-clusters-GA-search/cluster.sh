26316c3040e36a100f558e426183a9d0f7c12fe0
JeppJeppsson <jepsalt@gmail.com>
2023-02-01 17:30:21 +0100

    a2t1 data

---
+++ Assignment 2/A2/7/Na-clusters-GA-search/cluster.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#SBATCH -A C3SE2023-2-2 # Project name
+#SBATCH -J cluster7 # Name of the job
+#SBATCH -N 1 # Amount of nodes
+#SBATCH -n 1 # Amount of cores per node
+#SBATCH -t 10:00:00 # Maximum run time
+#SBATCH -o std.out # All non-error output
+#SBATCH -e err.out # All error-related output
+
+# Unload modules
+module purge
+
+# Load necessary modules
+module load ASE/3.22.1-foss-2022a
+module load GPAW/22.8.0-foss-2022a
+
+# Run the program
+srun gpaw python ga.py
