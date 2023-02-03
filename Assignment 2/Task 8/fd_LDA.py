27dae2888350f7705c8c6a6f9521ede66028cbd9
JeppJeppsson <jepsalt@gmail.com>
2023-02-03 01:01:04 +0100

    a

--- Assignment 2/Task 8/fd_LDA.py
+++ Assignment 2/Task 8/fd_LDA.py
@@ -2,7 +2,6 @@
 import time
 start_time = time.time()
 
-import numpy as np
 import gpaw as gp
 from gpaw import GPAW, PW
 from ase import Atoms
@@ -15,15 +14,15 @@ deca = read('Na6-structures/christmas-tree.xyz')
 tree = read('Na6-structures/half-decahedron.xyz')
 
 # Setting up the GPAW calculator
-calc_deca = gp.GPAW(mode='fd',xc='LDA',txt='fd_LDA_deca.txt')
-calc_tree = gp.GPAW(mode='fd',xc='LDA',txt='fd_LDA_tree.txt')
+calc_deca = gp.GPAW(mode='fd',xc='LDA',txt='fd_LDA/fd_LDA_deca.txt')
+calc_tree = gp.GPAW(mode='fd',xc='LDA',txt='fd_LDA/fd_LDA_tree.txt')
 
 deca.set_calculator(calc_deca)
 tree.set_calculator(calc_tree)
 
 # Optimizing
-opt_deca = BFGS(deca,logfile='bfgs_deca.log')
-opt_tree = BFGS(tree,logfile='bfgs_tree.log')
+opt_deca = BFGS(deca,logfile='fd_LDA/bfgs_deca.log')
+opt_tree = BFGS(tree,logfile='fd_LDA/bfgs_tree.log')
 
 opt_deca.run(fmax=.05)
 opt_tree.run(fmax=.05)
