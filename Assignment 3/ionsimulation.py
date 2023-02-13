7c063403a95a7a2db5dd3611630437f0f786af85
JeppJeppsson <jepsalt@gmail.com>
2023-02-13 11:58:21 +0100

    3.1 data

--- Assignment 3/ionsimulation.py
+++ Assignment 3/ionsimulation.py
@@ -1,10 +1,9 @@
 from gpaw import GPAW
-from ase import Atoms
 from ase.io import read, Trajectory
-from ase.units import fs,kB
+from ase.units import fs
 from ase.md.npt import NPT
 
-atoms = read('startconfig.xyz')
+atoms = read('pepsi.xyz')
 
 calc = GPAW(
     mode = 'lcao',
@@ -19,13 +18,13 @@ atoms.set_calculator(calc)
 dyn = NPT(
     atoms,
     temperature_K = 350,
-    timestep = .5*fs, # This is not an appropriate timestep , I can tell you that !
-    ttime = 20*fs, # Don ’t forget the fs!
-    externalstress = 0, # We don ’t use the barostat , but this needs to be set anyway !
+    timestep = .5*fs,
+    ttime = 20*fs,
+    externalstress = 0,
     logfile = 'mdOutput.log')
 
 trajectory = Trajectory('someDynamics.traj','w',atoms)
 
-dyn.attach(trajectory.write, interval=1) # Write the current positions etc. to file each timestep
+dyn.attach(trajectory.write, interval=1)
 
-dyn.run(4000) # Run 10 steps of MD simulation
+dyn.run(4000)
